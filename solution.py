3efeead8cda0113903a0df6659046ae7c021b441
NIKITA LOGINOV <80138722+morenekitov@users.noreply.github.com>
2023-04-06 00:58:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 287133833 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    x =  2*x / (88 * 88) 
+    loc = x.mean()
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return loc # Ваш ответ
